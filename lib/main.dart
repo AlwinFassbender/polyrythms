69fbbf83221aa41dff412096b4009bcc2c277271
AlwinFassbender <alwin.fassbender@gmail.com>
2023-12-07 02:32:46 +0100

    change name of app

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class PolyrythmApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Polyrythms',
       home: const SoundpoolInitializer(),
       theme: ThemeData(
         useMaterial3: true,
